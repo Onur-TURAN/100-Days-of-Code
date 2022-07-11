16c6cbaa097c994b4a2a7647bbc009cf4a037012
biyik <biyik@kali.kali>
2022-07-11 23:32:01 +0300

    exercise-2______try

---
+++ day_1/1-exercise_2/main.py
@@ -0,0 +1,18 @@
+
+""" bize anlattıgı küçük konu anlatımını da aşağıda uygulayalım"""
+
+print ( 'String Concatenation is done with the "+" sign.') # burada çift tırnak yazabilmek için print içeirisndeki string ifadesini tek tırnakta alarak hata almamayı başardım.
+print ( 'e.g. print("Hello " + "world")')
+print ( "New lines can be created with a backslash and n.")
+
+"""" hadi yukarıdaki 'string' in bizden istediğini aşağıya yazalım."""
+
+print ( "------\n")
+
+
+print ( 'Hello ' + 'world\n' 'I ' + 'am ' + 'computer.\n') # her tırnak sonundaki 1 karekter boşluklara dikkat. 
+
+
+
+
+
